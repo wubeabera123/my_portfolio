e1ccbe808a4414e1cfa42375b0a2337e2289ad25
wubu <wubeabera123@gmail.com>
2023-07-29 03:15:59 +0300

    navigaton items change

--- src/Landing.js
+++ src/Landing.js
@@ -4,35 +4,36 @@ import logo from "./Image/logo.png";
 
 function Landing(){
     return(
-    <nav>
-        <div className="container">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container mx-auto">
            <img src={logo} style={{
             width: "10rem",
             height: "3rem",
-            marginTop: "2rem"
+            borderRadius: "100rem"
             }} className="logo"/>
         </div>
-        <div className="container ">
+        <div className="container mx-auto ">
            <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link" href="#">About me</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About me</a>
+                <a className="nav-link" href="#">Services</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About me</a>
+                <a className="nav-link" href="#">Portfolio</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About me</a>
+                <a className="nav-link" href="#">Blog</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About me</a>
+                <a className="nav-link" href="#">Contacts</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About me</a>
+                <button className="btn btn-danger">Download CV</button>
               </li>
            </ul>
+           
         </div>
     </nav>
     );
