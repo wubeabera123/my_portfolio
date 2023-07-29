2b71d2f32bfc5e85e844ce3094ffbbccd6124f03
wubu <wubeabera123@gmail.com>
2023-07-29 23:49:11 +0300

    icons

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,33 @@
 import React from "react";
+import {RiAwardFill} from "react-icons/ri";
+import {BsPeopleFill} from "react-icons/bs";
+import {GiCalendarHalfYear} from "react-icons/gi";
+import {GoProjectSymlink} from "react-icons/go";
 function Info(){
     return(
-       <div className="info bg-dark-purple">
-          
+       <div id="info">
+          <div style={{marginTop: "-10rem"}} className="container d-flex justify-content-between" id="values">
+            <section id="bord1">
+                <RiAwardFill color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>02</h1>
+                <h3 style={{textAlign: "center", color: "white"}}>Awards</h3>
+            </section>
+            <section id="bord2">
+                <BsPeopleFill color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>50+</h1>
+                <h3 style={{textAlign: "center", color: "white"}}>Cleints</h3>
+            </section>
+            <section id="bord3">
+                <GiCalendarHalfYear color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/> 
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>03</h1>
+                <h3 style={{textAlign: "center", color: "white"}}>Years <br/>Experience</h3>
+            </section>
+            <section id="bord4">
+                <GoProjectSymlink color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/> 
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>80+</h1>
+                <h3 style={{textAlign: "center", color: "white"}}>Projects</h3>
+            </section>
+          </div>
        </div>
     )
 }
