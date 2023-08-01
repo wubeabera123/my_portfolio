84f67b84dddb2ac9b6e0c91f5153106ad399d60a
wubu <wubeabera123@gmail.com>
2023-08-01 13:40:57 +0300

    alignnment

--- src/Landing.js
+++ src/Landing.js
@@ -10,7 +10,7 @@ import {FaLinkedinIn} from "react-icons/fa";
 function Landing(){
     return(
   <div>    
-    <nav className="container navbar navbar-expand-lg d-flex">
+    <nav className="container navbar navbar-expand flex flex-nowrap">
         <div className="container mx-auto">
            <img src={logo} style={{
             width: "10rem",
@@ -18,7 +18,7 @@ function Landing(){
             borderRadius: "100rem"
             }} className="logo"/>
         </div>
-        <div className="container mx-auto ">
+        <div className="container">
            <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link" href="#">About me</a>
@@ -36,14 +36,13 @@ function Landing(){
                 <a className="nav-link" href="#">Contacts</a>
               </li>
               <li className="nav-item">
-                <button className="btn btn-danger">Download CV</button>
+                <button className="btn btn-danger" style={{height: "3rem",width: "8rem",fontSize: "small"}}>Download CV</button>
               </li>
            </ul>
-           
         </div>
     </nav>
-   <div className=" container d-flex justify-content-between">
-    <div className="container " style={{marginTop:"6rem"}}>
+   <div className="container" style={{display: "flex"}}>
+    <div className="container " style={{marginTop:"8rem"}}>
        <button className="btn btn-danger" style={{
         width: "8rem",
         height: "2.3rem",
@@ -54,20 +53,21 @@ function Landing(){
        <h6 style={{color: "white"}}>Frontend Developer</h6>
        <p style={{color: "white", marginTop: "2rem"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/> Nullam a nisl vel mauris egestas bibendum. Sed hendrerit,<br/> ex vel faucibus gravida, tellus sapien vestibulum velit</p>
     </div>
-    <div className="container">
+    <div style={{display: "flex"}}>
+    <div className="container" style={{marginRight: "-8rem"}}>
        <img src={person} style={{
          width: "24rem",
          marginTop: "4rem",
-         
        }}/>
     </div>
-    <div className="container">
-       <h5 className="vertical-text" style={{color: "red", transform: "rotate(90deg)", whiteSpace: "nowrap", marginTop: "14rem"}}>FOLLOW ME ON: </h5>
-       <section className="d-flex flex-column py-5" style={{marginLeft: "9.8rem"}}>
-        <a href="https://www.facebook.com/" ><MdFacebook color = "red"/></a>
+    <div style={{marginTop: "10rem"}}>
+       <h6 className="vertical-text" style={{color: "red", transform: "rotate(90deg)", whiteSpace: "nowrap"}}>FOLLOW ME ON</h6> 
+       <section className="d-flex flex-column" style={{marginTop: "7rem",marginLeft: "10rem"}}>
+        <a href="https://www.facebook.com/"><MdFacebook color = "red"/></a>
         <a href="#"><FaTwitter color = "red"/></a>
         <a href="#"><FaLinkedinIn color="red"/></a>
        </section>
+    </div>
     </div>
   </div> 
 </div> 
