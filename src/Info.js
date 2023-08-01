8a420fa7eac5ff96fdde5361200fe9f57a947ebd
wubu <wubeabera123@gmail.com>
2023-08-01 17:53:14 +0300

    dd

--- src/Info.js
+++ src/Info.js
@@ -9,22 +9,22 @@ function Info(){
           <div style={{marginTop: "-10rem"}} className="container d-flex justify-content-between" id="values">
             <section id="bord1">
                 <RiAwardFill color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/>
-                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>02</h1>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>01</h1>
                 <h3 style={{textAlign: "center", color: "white"}}>Awards</h3>
             </section>
             <section id="bord2">
                 <BsPeopleFill color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/>
-                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>50+</h1>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>01</h1>
                 <h3 style={{textAlign: "center", color: "white"}}>Cleints</h3>
             </section>
             <section id="bord3">
                 <GiCalendarHalfYear color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/> 
-                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>03</h1>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>01</h1>
                 <h3 style={{textAlign: "center", color: "white"}}>Years <br/>Experience</h3>
             </section>
             <section id="bord4">
                 <GoProjectSymlink color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/> 
-                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>80+</h1>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>10+</h1>
                 <h3 style={{textAlign: "center", color: "white"}}>Projects</h3>
             </section>
           </div>
