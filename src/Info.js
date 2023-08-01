906e508f6a4c6bd947235801ffdfb0fb72585393
wubu <wubeabera123@gmail.com>
2023-08-01 14:01:00 +0300

    projects made

--- src/Info.js
+++ src/Info.js
@@ -19,12 +19,12 @@ function Info(){
             </section>
             <section id="bord3">
                 <GiCalendarHalfYear color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/> 
-                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>03</h1>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>01</h1>
                 <h3 style={{textAlign: "center", color: "white"}}>Years <br/>Experience</h3>
             </section>
             <section id="bord4">
                 <GoProjectSymlink color="red" style={{fontSize: "2rem", marginTop: "-2rem",marginLeft: "1rem"}}/> 
-                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>80+</h1>
+                <h1 style={{textAlign: "center", color: "white", fontWeight: "bold"}}>10+</h1>
                 <h3 style={{textAlign: "center", color: "white"}}>Projects</h3>
             </section>
           </div>
