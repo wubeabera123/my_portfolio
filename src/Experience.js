8d7296d955f257b79fbbb714a03e573e876ece8b
wubu <wubeabera123@gmail.com>
2023-08-04 02:08:10 +0300

    experience lists

--- src/Experience.js
+++ src/Experience.js
@@ -8,25 +8,32 @@ function Experience(){
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan leo quis metus efficitur, non euismod nulla gravida. Nulla facilisi.<br/> Duis imperdiet consequat mauris, eget fermentum ante commodo vel.</p>
             </div>
             <div className="container py-3 d-flex justify-content-between" style={{color: "white"}}>
-              <div>
+             <div style={{borderLeft: "0.2rem solid rgb(90,5,5)",height: "7rem"}}>
+              <div style={{marginLeft: "1rem"}}>
                 <h4>Step 01</h4>
                 <p>Lorem ipsum dolor sit amet,<br/> consectetur adipiscing elit. <br/>Sed accumsan leo quis metus.</p>
               </div>
-              <div>
+             </div>
+             <div style={{borderLeft: "0.2rem solid rgb(90,5,5)",height: "7rem"}}>
+              <div style={{marginLeft: "1rem"}}>
                 <h4>Step 01</h4>
                 <p>Lorem ipsum dolor sit amet,<br/> consectetur adipiscing elit. <br/>Sed accumsan leo quis metus.</p>
               </div>
-              <div>
+             </div>
+             <div style={{borderLeft: "0.2rem solid rgb(90,5,5)",height: "7rem"}}>
+              <div style={{marginLeft: "1rem"}}>
                 <h4>Step 01</h4>
                 <p>Lorem ipsum dolor sit amet,<br/> consectetur adipiscing elit. <br/>Sed accumsan leo quis metus.</p>
               </div>
+             </div>
             </div>
-            <div className="container py-3" style={{color: "white"}}>
-              <div className="d-inline-block" style={{marginRight: "10rem"}}>
+            
+            <div className="d-flex justify-content-evenly" style={{color: "white"}}>
+              <div className="d-inline-block" style={{marginRight: "14rem",marginLeft: "20rem",borderLeft: "0.2rem solid rgb(90,5,5)",height: "7rem"}}>
                 <h4>Step 01</h4>
                 <p>Lorem ipsum dolor sit amet,<br/> consectetur adipiscing elit. <br/>Sed accumsan leo quis metus.</p>
               </div>
-              <div className="d-inline-block" style={{marginRight: "10rem"}}>
+              <div className="d-inline-block" style={{marginRight: "16rem",borderLeft: "0.2rem solid rgb(90,5,5)",height: "7rem"}}>
                 <h4>Step 01</h4>
                 <p>Lorem ipsum dolor sit amet,<br/> consectetur adipiscing elit. <br/>Sed accumsan leo quis metus.</p>
               </div>
