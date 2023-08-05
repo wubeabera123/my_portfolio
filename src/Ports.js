19b8ec666abc238d1f60c2fdd070cc56ec1f1ed0
wubu <wubeabera123@gmail.com>
2023-08-05 14:11:03 +0300

    responsive

--- src/Ports.js
+++ src/Ports.js
@@ -8,13 +8,13 @@ import f from "./Image/f.png";
 function Ports(){
     return(
       <div>
-        <div className="container d-flex justify-content-round">
-          <div className="container" style={{marginTop: "6rem"}}>
+        <div className="container col-sm-12 d-flex justify-content-round">
+          <div className="container" style={{marginTop: "6rem",width: "50%"}}>
              <p id="port">Portfolio</p>
              <p style={{color: "white"}}>Preparing portfolio lorem pages fonts <br/> pages lorem portfolio Preparing</p>
           </div>
           
-          <div class="btn-group" style={{height: "2.8rem",width: "60rem",marginTop: "10rem"}}>
+          <div class="btn-group" style={{height: "2.8rem",width: "60rem",marginTop: "10rem",width:"50%"}}>
              <button type="button" class="btn btn-danger" style={{fontSize: "0.8rem"}}>All Work</button>
              <button type="button" class="btn btn-primary" style={{backgroundColor: "#211930",borderColor: "#211930",fontSize: "0.8rem"}}>Personal Projects</button>
              <button type="button" class="btn btn-primary" style={{backgroundColor: "#211930",borderColor: "#211930",fontSize: "0.8rem"}}>Proffesional Projects</button>
